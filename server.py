ced1a0ebcc8527afbb4ee592fe17e546247e204f
Danmei Chen <danmei.chen@schneider-electric.com>
2017-08-30 16:48:12 +0200

    change server to run test in multiprocess

--- server.py
+++ server.py
@@ -11,46 +11,40 @@ http://www.culte.org/listes/linux-31/2007-10/msg00008.html
 import socket
 import time
 import pickle
+from multiprocessing import Process
 
-N_PAQUET = 500
+N_PAQUET = 100
 PAQUET = b"t"
 T_PAQUET = 10
-DESTINATION = ('192.168.42.1', 10002)
+DESTINATION = [(('192.168.42.1', 10002),5556)]
 KO = 1024
 PING_MSG_SIZE = 2
 
 
+def get_debit(port_PC, destination):
+    soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    soc.bind(('', port_PC))
 
-class handle():
-    def __init__(self):
-        y = (1970, 1, 1, 1, 0, 0, 0, 0, 0)
-        self.y = time.mktime(y)
+    mss = PAQUET * T_PAQUET
+    soc.sendto(pickle.dumps([N_PAQUET, T_PAQUET]), destination)
+    y = time.time()
+    for j in range(N_PAQUET):
+        soc.sendto(mss, destination)
+        soc.recvfrom(PING_MSG_SIZE)
+    t = time.time()-y
+    v = N_PAQUET * T_PAQUET / t
 
-    def restart(self):
-        self.y = time.time()
+    print("Nombre de paquet", N_PAQUET, ", taille de paquet", T_PAQUET, "octets, destination", destination)
+    print("                                                   ------>", v, "O/s")
 
-    def get_value(self):
-        x = time.time()
 
-        resultat = x - self.y
-        return resultat
-
-    def get_debit(self):
-        soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        soc.bind(('', 5556))
-
-        mss = PAQUET*T_PAQUET
-        soc.sendto(pickle.dumps([N_PAQUET, T_PAQUET]), DESTINATION)
-        self.restart()
-        for j in range(N_PAQUET):
-            soc.sendto(mss, DESTINATION)
-            soc.recvfrom(PING_MSG_SIZE)
-        t = self.get_value()
-        v=N_PAQUET*T_PAQUET/t
-
-        print("Nombre de paquet",N_PAQUET, ", taille de paquet", T_PAQUET, "octets, destination", DESTINATION)
-        print("                                                   ------>", v, "O/s")
+def main():
+    processes = None
+    for dest in DESTINATION:
+        test = Process(target=get_debit, args=(dest[1], dest[0]))
+        #test.daemon = True
+        test.start()
 
 if __name__ == '__main__':
-    server = handle()
-    server.get_debit()
+    main()
+
